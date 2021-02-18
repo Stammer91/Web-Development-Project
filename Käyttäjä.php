a92ef1d944c348ce001ed1308cbc9867548f5d39
Stammer91 <73183215+Stammer91@users.noreply.github.com>
2021-02-18 10:55:57 +0200

    Taulut Kirjautumiseen (kesken)

--- Käyttäjä.php
+++ Käyttäjä.php
@@ -26,5 +26,73 @@
 
           echo " Kirjautuminen onnistui"; 
 ?>
+//Varaukset
+<?php
+set_error_handler("anyError", E_ALL);
+
+ 
+$initials=parse_ini_file(".ht_db_inifile.ini");
+$database=$initials["databasename"];
+$user=$initials["username"];
+$passwd=$initials["password"];
+
+
+$nimi=$_POST["tilaaja_nimi"];
+$email=$_POST["tilaaja_mail"];
+$maara=$_POST["tilaaja_maara"];
+$pvm=$_POST["tilaaja_aika"];
+$lisatiedot=$_POST["tilaaja_msg"];
+
+$yhteys=mysqli_connect("localhost", $user, $passwd);
+
+if (!$yhteys){
+    $error="Yhteys tietokantapalvelimeen epäonnistui";
+    print $error;
+    return;
+}
+$ok=mysqli_select_db($yhteys, $database);
+if (!$ok){
+    $error="Tietokannan valinta epäonnistui";
+    print $error;
+    return;
+}
+$tulos=mysqli_query($yhteys, "select * from ryhmä5_varaukset");
+while ($rivi = mysqli_fetch_object($tulos)){
+  print "$rivi->id $rivi->nimi $rivi->email $rivi->maara $rivi->pvm $rivi->lisatiedot";
+}
+?>
+//Palautteet
+<?php
+set_error_handler("anyError", E_ALL);
+
+ 
+$initials=parse_ini_file(".ht_db_inifile.ini");
+$database=$initials["databasename"];
+$user=$initials["username"];
+$passwd=$initials["password"];
+
+$nimi=$_POST["nimi"];
+$email=$_POST["email"];
+$viesti=$_POST["viesti"];
+
+$yhteys=mysqli_connect("localhost", $user, $passwd);
+
+if (!$yhteys){
+    $error="Yhteys tietokantapalvelimeen epäonnistui";
+    print $error;
+    return;
+}
+$ok=mysqli_select_db($yhteys, $database);
+if (!$ok){
+    $error="Tietokannan valinta epäonnistui";
+    print $error;
+    return;
+}
+$tulos=mysqli_query($yhteys, "select * from ryhma5_palautteet");
+while ($rivi = mysqli_fetch_object($tulos)){
+  print "$rivi->id $rivi->nimi $rivi->email $rivi->viesti";
+}
+
+?>
 </body>
 </html>
